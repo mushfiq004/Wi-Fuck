29ce3e1268b091abc84d9d801aaf9996f6584f48
Mushfiqur Rahman (mxrx.04) <87242454+mushfiqrhmn@users.noreply.github.com>
2024-01-12 01:21:03 +0600

    wi-fuck.py

--- wi-fuck.py
+++ wi-fuck.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Modify History : rofl0r => Biri_B@B@ => Mohammad_Alamin (Toxinum) => Mushfiqur Rahman
+# Modify History : rofl0r => Biri_B@B@ => Mohammad_Alamin (Toxinum) => Shourov => Mushfiqur Rahman
 # VERSION 1.0.1
 # Open Source Code.No Need More Modification.
 import sys, subprocess, os, tempfile, shutil, re, codecs, socket, pathlib, time, collections, statistics, collections, csv, http.client, marshal
